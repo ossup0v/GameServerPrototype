4bf3bf739ff8d2d05f312dda7627a82458feed70
mikhail <ossup0v@gmail.com>
2022-09-16 00:51:01 +0400

    add shared as submodule

--- src/ServerPrototype.Shared/Extensions/DebugExtensions.cs
+++
@@ -1,7 +0,0 @@
-﻿public static class DebugExtensions
-{
-    public static string ToDebugString<T>(T[] array, string seporator = " ")
-    {
-        return string.Join(seporator, array);
-    }
-}
--- src/ServerPrototype.Shared/Packets/Generated/generate_cmd.bat
+++
@@ -1,1 +0,0 @@
-mpc -i "../" -o "generated.cs"
